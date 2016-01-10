d78944690d68e91948e82319d1e233e6b31524ab
xiad <xia.ldj@gmail.com>
2016-01-10 16:49:57 +0800

    di, loadmodules

--- app/app.js
+++ app/app.js
@@ -5,8 +5,21 @@
 
 function genChart(id) {
   var c1 = document.getElementById(id + 'Code');
-  var digest = flowchart.parse(c1.value);
-  digest.drawSVG(id);
+  var chart = flowchart.parse(c1.value);
+  chart.drawSVG(id);
+}
+
+// **********************************
+
+
+// **********************************
+// diagra
+// **********************************
+
+function genDiagram(id) {
+  var d1 = document.getElementById(id+'Code');
+  var diagram = Diagram.parse(d1.value);
+  diagram.drawSVG(id, {theme: 'hand'});
 }
 
 // **********************************
